49ef2149e30cc4076bc1775c4b2fddaddd90aa19
Amaryllis750 <danielchukwuemela751@gmail.com>
2026-04-16 14:12:27 +0100

    feat: implemented CRUD for profile route

---
+++ src/db/conn.ts
@@ -0,0 +1,27 @@
+import { Pool } from "pg";
+import dotenv from 'dotenv';
+
+dotenv.config({quiet: true});
+let pool: Pool | undefined;
+
+const getPool = () => {
+    if(pool) return pool;
+
+    const {DB_HOST, DB_PASSWORD, DB_NAME, DB_USER, DB_PORT} = process.env;
+
+    if(!DB_HOST || !DB_PASSWORD || !DB_NAME || !DB_USER || !DB_PORT){
+        throw new Error("Database credentials are not defined");
+    }
+
+    pool = new Pool ({
+        host: DB_HOST, 
+        user: DB_USER, 
+        password: DB_PASSWORD, 
+        port: Number(DB_PORT), 
+        database: DB_NAME
+    });
+
+    return pool;
+}
+
+export {getPool};
