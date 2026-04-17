22e5a4a55ab0e5f498976cd8ddb4068a210b4472
Amaryllis750 <danielchukwuemela751@gmail.com>
2026-04-17 03:07:37 +0100

    feat: enabled db connection with DB_URL

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -7,7 +7,12 @@ let pool: Pool | undefined;
 const getPool = () => {
     if(pool) return pool;
 
-    const {DB_HOST, DB_PASSWORD, DB_NAME, DB_USER, DB_PORT} = process.env;
+    const {DB_HOST, DB_PASSWORD, DB_NAME, DB_USER, DB_PORT, DB_URL} = process.env;
+
+    if(DB_URL){
+        pool = new Pool({connectionString: DB_URL, ssl: {rejectUnauthorized: false}});
+        return pool;
+    }
 
     if(!DB_HOST || !DB_PASSWORD || !DB_NAME || !DB_USER || !DB_PORT){
         throw new Error("Database credentials are not defined");
